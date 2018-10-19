90bb6ccfb9b8473f7ea73d8bd4aefc15ae7d3ef7
Michael Duffey <mduffey07@gmail.com>
2018-10-19 17:01:07 -0500

    Added what will probably be the only MVC controller to the application as well as the very, very basic template of the page.

---
+++ Website.NET/Site/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Website.NET/Site/Startup.cs
+++ Website.NET/Site/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Site
 {
@@ -15,19 +18,35 @@ namespace Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //if (context.Request.Path == "/")
+                //{
+                //    context.Response.Headers.Add("Cache-Control", "public, max-age=600");
+                //    var file = env.WebRootFileProvider.GetFileInfo("index.html");
+                //    using (var streamReader = new StreamReader(file.CreateReadStream()))
+                //    {
+                //        await context.Response.WriteAsync(streamReader.ReadToEnd());
+                //    }
+                //}
             });
         }
     }
