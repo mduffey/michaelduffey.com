8663909ffac730f4e06a7c7fccff47b51a84dab6
Michael Duffey <mduffey07@gmail.com>
2018-10-19 19:34:52 -0500

    Adding the WWWCERT that theoretically holds the cert in Linux as an environment variable to look for a cert.

--- Website.NET/Site/Program.cs
+++ Website.NET/Site/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -17,8 +20,22 @@ namespace Site
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var certPath = Environment.GetEnvironmentVariable("WWWCERT");
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        if (string.IsNullOrWhiteSpace(certPath))
+                            listenOptions.UseHttps();
+                        else
+                            listenOptions.UseHttps(certPath);
+                    });
+
+                })
                 .UseStartup<Startup>();
+        }
     }
 }
--- Website.NET/Site/Startup.cs
+++ Website.NET/Site/Startup.cs
@@ -29,24 +29,11 @@ namespace Site
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
-            });
-            app.Run(async (context) =>
-            {
-                //if (context.Request.Path == "/")
-                //{
-                //    context.Response.Headers.Add("Cache-Control", "public, max-age=600");
-                //    var file = env.WebRootFileProvider.GetFileInfo("index.html");
-                //    using (var streamReader = new StreamReader(file.CreateReadStream()))
-                //    {
-                //        await context.Response.WriteAsync(streamReader.ReadToEnd());
-                //    }
-                //}
             });
         }
     }
