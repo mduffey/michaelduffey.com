33c37a29ae1d21f999977a3178d0fb85357b2e84
Michael Duffey <mduffey07@gmail.com>
2018-10-19 20:08:54 -0500

    Removing URLs from the profiles, since we have to override Kestrel with the SSL certs. Updating Program to handle a new process for pulling the cert.

--- Website.NET/Site/Program.cs
+++ Website.NET/Site/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -22,20 +23,36 @@ namespace Site
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            var certPath = Environment.GetEnvironmentVariable("WWWCERT");
-            return WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(options =>
-                {
-                    options.Listen(IPAddress.Any, 443, listenOptions =>
-                    {
-                        if (string.IsNullOrWhiteSpace(certPath))
-                            listenOptions.UseHttps();
-                        else
-                            listenOptions.UseHttps(certPath);
-                    });
 
-                })
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(BuildOptions)
                 .UseStartup<Startup>();
+        }
+
+        private static void BuildOptions(KestrelServerOptions options)
+        {
+            var certPath = Environment.GetEnvironmentVariable("WWWCERT");
+
+            var certificate = !string.IsNullOrWhiteSpace(certPath)
+                ? new X509Certificate2(certPath, Environment.GetEnvironmentVariable("WWWKEY"))
+                : null;
+            var environment = (IHostingEnvironment)options.ApplicationServices.GetService(typeof(IHostingEnvironment));
+            if (environment.IsDevelopment())
+            {
+                options.Listen(IPAddress.Loopback, 5000);
+                options.Listen(IPAddress.Loopback, 5001, listenOptions =>
+                {
+                    listenOptions.UseHttps();
+                });
+            }
+            else
+            {
+                options.Listen(IPAddress.Any, 80);
+                options.Listen(IPAddress.Any, 443, listenOptions =>
+                {
+                    listenOptions.UseHttps(certificate);
+                });
+            }
         }
     }
 }
