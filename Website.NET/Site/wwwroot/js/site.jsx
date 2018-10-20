da7b31e2cbbdb97848a8ed682ca3bf9c1ad5c542
Michael Duffey <mduffey07@gmail.com>
2018-10-20 15:15:26 -0500

    Adding a jsx file and portions of the React tutorial to the site to start understanding React's process.

---
+++ Website.NET/Site/wwwroot/js/site.jsx
@@ -0,0 +1,48 @@
+﻿class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commontBox">
+                <p>This is really weird.</p>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        return (
+            <div className="commentList">
+                <Comment author="Daniel Lo Nigro">
+                    Hello ReactJS.NET World!
+                </Comment>
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">
+                    This is another comment
+                </Comment>
+            </div>
+        );
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
