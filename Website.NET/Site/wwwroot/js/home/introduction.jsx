09512e5f8741ab1fc7d064f1a5ff2b64bdfc3a1d
Michael Duffey <mduffey07@gmail.com>
2018-10-21 15:41:58 -0500

    Pushing JSX files to folders based on the area they represent. Completed the code for handling the introduction.

---
+++ Website.NET/Site/wwwroot/js/home/introduction.jsx
@@ -0,0 +1,40 @@
+﻿class Introduction extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "", description: "" };
+    }
+    componentDidMount() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ title: data.title, description: data.description });
+        };
+        xhr.send();
+    }
+    render() {
+        return (
+            <div>
+                <h2>{this.state.title}</h2>
+                <Paragraphs data={this.state.description} />
+            </div>
+        );
+    }
+}
+
+class Paragraphs extends React.Component {
+    render() {
+        if (this.props.data != '')
+        {
+            // The key is the index since we're splitting the paragraph up ourselves.
+            let split = this.props.data.split("\n").map((i, key) => <p key={key}>{i}</p>);
+            return (<div>{ split }</div>);
+        }
+        return null;
+    }
+}
+
+ReactDOM.render(
+    <Introduction url="/home/content" />,
+    document.getElementById('content')
+);
